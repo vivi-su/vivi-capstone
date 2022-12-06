d0869e7d68432e5ead32743aaf19c404a488f619
Vivi Schulz <haweirh@gmail.com>
2022-12-06 11:09:59 -0800

    Delete public folder

--- src/components/top-header/TopHeader.js
+++
@@ -1,8 +0,0 @@
-import {Link} from "react-router-dom";
-
-
-export default function TopHeader (){
-    return(
-         <Link to={"disney"}>Check out Disney</Link>
-    );
-}
--- src/pages/disney/DisneyPage.js
+++
@@ -1,8 +0,0 @@
-import {Link} from "react-router-dom";
-import "./DisneyPage.scss";
-export default function DisneyPage(){
-
-    return (
-    <Link to={"/play"}><button className="Disney__back">Back to play</button></Link>
-    );
-}
