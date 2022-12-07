69d7c8c0cdd3cf56eebe7c131d453050b610baf1
Vivi Schulz <haweirh@gmail.com>
2022-12-07 00:01:56 -0800

    Created disney page and fetch all data

--- client/src/pages/disney/DisneyPage.js
+++ client/src/pages/disney/DisneyPage.js
@@ -1,8 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 import "./DisneyPage.scss";
 export default function DisneyPage(){
 
+    const [items, setItems]=useState([]);
+
+    useEffect(() => {
+        
+      const DISNEY_URL = `https://api.disneyapi.dev/characters/`;
+      axios.get(DISNEY_URL)
+            .then(({ data }) => {
+              setItems(data.data);
+            });
+            }, []);
     return (
-    <Link to={"/play"}><button className="Disney__back">Back to play</button></Link>
+      <>
+        <main className="disney">
+          <Link to={"/play"}>
+            <button>Back to play</button>
+          </Link>
+          {items.map((item) => (
+            <div key={uuidv4()}>
+            <p>{item.name}</p>
+              <img
+                src={item.imageUrl}
+                alt={item.name}
+                className="disney__cartoon-img"
+              ></img>
+            </div>
+          ))}
+        </main>
+      </>
     );
 }
--- client/src/pages/home-page/HomePage.js
+++ client/src/pages/home-page/HomePage.js
@@ -1,24 +1,77 @@
+import { useEffect, useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+
+import axios from "axios";
 import TopHeader from "../../components/top-header/TopHeader";
 import PlayHeader from "../../components/play-header/PlayHeader";
-import { Outlet, Link } from "react-router-dom";
 import "./HomePage.scss";
 
+const BACK_END = process.env.REACT_APP_BACKEND_URL;
+
 export default function HomePage() {
+  const [jokes, setJokes] = useState([]);
+  const [joke, setJoke] = useState("");
+  const [show,setShow] =useState(false);
+
+  useEffect(() => {
+    const URL = `${BACK_END}/api/jokes`;
+
+    axios
+      .get(URL)
+      .then(({ data }) => {
+        const randomJoke = data[Math.floor(Math.random() * data.length)];
+        setJoke(randomJoke.joke);
+        setJokes(data);
+        setShow(false)
+      })
+      .catch((err) => console.log(err));
+  }, []);
   
+ 
+  const getJoke =()=>{
+    if(jokes){
+    const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+    setJoke(randomJoke.joke);
+    setShow(true);
+    }
+  }
+
+
+
   return (
     <>
       <main className="home">
         <section className="home__top">
-          <TopHeader />
-          <p>Joke</p>
+          <div className="home__half">
+            <div className="home__left">
+              <TopHeader />
+            </div>
+            <AnimatePresence>
+              {show && (
+                <motion.p
+                  className="home__joke"
+                  key="joke"
+                  initial={{ x: "50%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: "50%", opacity: 0, transition:{duration:0.1} }}
+                  transition={{ duration: 0.2, ease: "easeOut" }}
+                >
+                  {joke}
+                </motion.p>
+              )}
+            </AnimatePresence>
+          </div>
           <Outlet />
           <PlayHeader />
 
-          <button>Give me a Joke</button>
+          <button onClick={() => getJoke()}>Give me a Joke</button>
+          {show && (
+            <button onClick={() => setShow(false)}>I don't want a Joke</button>
+          )}
           <Link to={"/"}>
             <button>Back</button>
           </Link>
-
         </section>
       </main>
     </>
