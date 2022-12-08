97ac3def9cbcd86006708b27c2655b786c4746e0
Vivi Schulz <haweirh@gmail.com>
2022-12-07 21:03:09 -0800

    Add form input for search in disney

--- client/src/pages/disney/DisneyPage.js
+++ client/src/pages/disney/DisneyPage.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { DISNEY_URL } from "../../utils/utils";
 import {selectedByID_URL} from "../../utils/utils";
@@ -7,13 +7,14 @@ import {selectedByID_URL} from "../../utils/utils";
 import "./DisneyPage.scss";
 import Disney from "../../components/disney/Disney";
 
-   
+const PAGE_DISNEY_URL =(num)=> DISNEY_URL+`?page=${num}`;
 
 export default function DisneyPage() {
   const [items, setItems] = useState([]);
   const [detail, setDetail] = useState({});
   const [show, setShow] = useState(false);
   const { disneyID } = useParams();
+  const formRef = useRef();
 
   useEffect(() => {
     axios.get(DISNEY_URL).then(({ data }) => {
@@ -25,12 +26,43 @@ export default function DisneyPage() {
     if(!disneyID) {return}
     axios.get(selectedByID_URL(disneyID)).then(({ data }) => {
      setDetail(data);
-    });
+    }).catch((err)=>console.log("Error",err));
   }, [disneyID]);
+
+  const handelSearchPage =(e)=>{
+    e.preventDefault();
+    const pageNumber = formRef.current.typePageInput.value;
+    axios.get(PAGE_DISNEY_URL(pageNumber))
+         .then(({data})=>{
+          setItems(data.data)
+         }).catch((err)=>{
+          console.log("Error", err);
+         })
+    formRef.current.reset();
+  }
+
+  const handelSearchName =(e)=>{
+    e.preventDefault();
+    const searchName = formRef.current.searchName.value;
+    const searcheditem = items.filter((item)=>item.name.includes(searchName));
+    setItems(searcheditem);
+    formRef.current.reset();
+  }
 
   return (
     <>
-        <Disney items={items} detail={detail} show={show} setShow={setShow} />
+      <form ref={formRef}>
+        <input type="number" name="typePageInput"></input>
+        <button onClick={handelSearchPage}>Search Page</button>
+        <input
+          type="search"
+          name="searchName"
+          minNumber={1}
+          maxNumber={149}
+        ></input>
+        <button onClick={handelSearchName}>Search Charactor</button>
+      </form>
+      <Disney items={items} detail={detail} show={show} setShow={setShow} />
     </>
   );
 }
